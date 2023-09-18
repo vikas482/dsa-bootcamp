636b367b72ce982b541c37ca1ae2a6846d46ddcf
vikas482 <vikas482@gmail.com>
2023-09-18 13:30:59 -0400

    adding

---
+++ dsa_math_problems/src/dsa_math_problems/CountDigits.java
@@ -0,0 +1,24 @@
+package dsa_math_problems;
+
+public class CountDigits {
+
+	static int countDigits(int number) {
+		
+		int count =0;
+		while(number>0) {
+			
+			number = number/10;
+			count++;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(CountDigits.countDigits(1234));
+		
+	}
+
+}
